f0be13d925cac53f405a436b4463631701a816b8
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-12 12:39:05 +0600

    Add documentation to hooks

--- src/react/hooks/use-acceleration-subscription.ts
+++ src/react/hooks/use-acceleration-subscription.ts
@@ -2,6 +2,12 @@ import { Motion } from "@capacitor/motion";
 import { saveMotionInfo } from "../../save-motion-info";
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
 
+/**
+ * This hook subscribes to the acceleration event and saves the data to Deep
+ * 
+ * @remarks
+ * It is not recommended to use this hook directly. Instead of {@link WithUseAccelerationSubscription}
+ */
 export function useAccelerationSubscription(param:UseAccelerationSubscriptionParam) {
    const { deep, deviceLinkId } = param;
    Motion.addListener('accel', async (accelData) => {
@@ -14,6 +20,12 @@ export function useAccelerationSubscription(param:UseAccelerationSubscriptionPar
 }
 
 export interface UseAccelerationSubscriptionParam {
+   /**
+    * A Deep client instance
+    */
    deep: DeepClient;
+   /**
+    * A device link id
+    */
    deviceLinkId: number;
 }
--- src/react/hooks/use-orientation-subscription.ts
+++ src/react/hooks/use-orientation-subscription.ts
@@ -2,6 +2,12 @@ import { Motion } from "@capacitor/motion";
 import { saveMotionInfo } from "../../save-motion-info";
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
 
+/**
+ * This hook subscribes to the orientation event and saves the data to Deep
+ * 
+ * @remarks
+ * It is not recommended to use this hook directly. Instead of {@link WithUseOrientationSubscription}
+ */
 export function useOrientationSubscription(param:UseOrientationSubscriptionParam) {
    const { deep, deviceLinkId } = param;
    Motion.addListener('orientation', async (orientationData) => {
@@ -16,6 +22,12 @@ export function useOrientationSubscription(param:UseOrientationSubscriptionParam
 }
 
 export interface UseOrientationSubscriptionParam {
+   /**
+    * A Deep client instance
+    */
    deep: DeepClient;
+   /**
+    * A device link id
+    */
    deviceLinkId: number;
 }
