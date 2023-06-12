16a2283505d5802e7f6134a99bec46229332fd8e
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-12 13:48:15 +0600

    Add info how to manually save motion info to docs

--- src/save-motion-info.ts
+++ src/save-motion-info.ts
@@ -62,6 +62,32 @@ const accelerationListener = await Motion.addListener(
     });
   }
 );
+#### Save motion info manually
+```ts
+let info = {
+  acceleration: {
+    x: 3.2,
+    y: 4.7,
+    z: -9.8,
+  },
+  accelerationIncludingGravity: {
+    x: 0.3,
+    y: 0.2,
+    z: -9.8,
+  },
+  rotationRate: {
+    alpha: 30,
+    beta: 60,
+    gamma: 90,
+  },
+  interval: 1000,
+};
+
+await saveMotionInfo({
+  deep,
+  deviceLinkId,
+  info,
+});
 ```
    */
 export async function saveMotionInfo(
