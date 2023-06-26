035a1bcc66f33f77cbad9d04f6a1e2c10761fc71
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-26 14:04:23 +0600

    Add LinkName enum

---
+++ src/link-name.ts
@@ -0,0 +1,17 @@
+export enum LinkName {
+   Motion,
+   MotionAcceleration,
+   MotionAccelerationX,
+   MotionAccelerationY,
+   MotionAccelerationZ,
+   MotionAccelerationIncludingGravity,
+   MotionAccelerationIncludingGravityX,
+   MotionAccelerationIncludingGravityY,
+   MotionAccelerationIncludingGravityZ,
+   MotionRotationRate,
+   MotionRotationRateAlpha,
+   MotionRotationRateBeta,
+   MotionRotationRateGamma,
+   MotionInterval,
+   HandleMotionUpdate
+}
