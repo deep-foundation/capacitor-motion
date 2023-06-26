35a1336a059e25cc4e597c1e1208d071574c053e
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-26 14:04:31 +0600

    Add example for MotionInfo

--- src/motion-info.ts
+++ src/motion-info.ts
@@ -1,3 +1,26 @@
 import { AccelListenerEvent } from "@capacitor/motion";
 
+/**
+ * @example
+```ts
+const motionInfo: MotionInfo = {
+  "acceleration": {
+    "x": 3.2,
+    "y": 4.7,
+    "z": -9.8
+  },
+  "accelerationIncludingGravity": {
+    "x": 0.3,
+    "y": 0.2,
+    "z": -9.8
+  },
+  "rotationRate": {
+    "alpha": 30,
+    "beta": 60,
+    "gamma": 90
+  },
+  "interval": 1000
+}
+```
+ */
 export type MotionInfo = AccelListenerEvent;
