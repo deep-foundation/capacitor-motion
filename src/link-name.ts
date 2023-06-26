5925e4d074111923e2cefd00f67a0abdf590a90e
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-26 14:46:35 +0600

    Add doc for LinkName

--- src/link-name.ts
+++ src/link-name.ts
@@ -1,3 +1,14 @@
+/**
+ * Contains the names of all links within this package. 
+ * 
+ * @example
+```ts
+const notifyTypeLinkId = await deep.id(
+   PACKAGE_NAME,
+   LinkName[LinkName.Notify]
+)
+```
+ */
 export enum LinkName {
    Motion,
    MotionAcceleration,
