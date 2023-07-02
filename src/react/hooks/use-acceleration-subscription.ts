bebdba74868a9f88026047a171154177d01fe9f9
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-02 22:40:31 +0600

    Add debugs, console logs, use getSubscriptionHandler

--- src/react/hooks/use-acceleration-subscription.ts
+++ src/react/hooks/use-acceleration-subscription.ts
@@ -1,39 +1,63 @@
-import { Motion } from "@capacitor/motion";
-import { getMotionValueUpdateSerialOperations } from "../../get-motion-value-update-serial-operationsa";
-import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
-import { useEffect } from "react";
+import { Motion } from '@capacitor/motion';
+import { getMotionValueUpdateSerialOperations } from '../../get-motion-value-update-serial-operationsa';
+import {
+  DeepClient,
+  SerialOperation,
+} from '@deep-foundation/deeplinks/imports/client';
+import { useEffect } from 'react';
+import { PACKAGE_NAME } from '../../package-name';
+import { LinkName } from '../../link-name';
+import { BoolExpLink } from '@deep-foundation/deeplinks/imports/client_types';
+import createDebugMessages from 'debug';
+import { getMotionInsertSerialOperations } from '../../get-motion-insert-serial-operations';
+import { getSubscriptionHandler } from '../get-subscription-handler';
 
 /**
  * This hook subscribes to the acceleration event and saves the data to Deep
- * 
+ *
  * @remarks
  * It is not recommended to use this hook directly. Instead of {@link WithUseAccelerationSubscription}
+ * 
+ * @example
+```ts
+useAccelerationSubscription({
+  deep,
+  containerLinkId
+})
+```
  */
-export function useAccelerationSubscription(param:UseAccelerationSubscriptionParam) {
-   const { deep, deviceLinkId } = param;
-   
-   useEffect(() => {
-      const accelerationHandler = Motion.addListener('accel', async (accelData) => {
-         await getMotionValueUpdateSerialOperations({
-           deep,
-           deviceLinkId,
-           info: accelData,
-         });
-       });
+export function useAccelerationSubscription(
+  param: UseAccelerationSubscriptionParam
+) {
+  const debug = createDebugMessages(
+    `${PACKAGE_NAME}:useAccelerationSubscription`
+  );
+  debug({ param });
+  const { deep, containerLinkId } = param;
 
-      return () => {
-         accelerationHandler.remove();
-      }
-   }, [deep, deviceLinkId]) 
+  useEffect(() => {
+    const accelerationHandlerFunction = getSubscriptionHandler({
+      deep,
+      containerLinkId,
+    });
+    const accelerationHandler = Motion.addListener(
+      'accel',
+      accelerationHandlerFunction
+    );
+    debug({ accelerationHandler });
+    return () => {
+      accelerationHandler.remove();
+    };
+  }, [deep, containerLinkId]);
 }
 
 export interface UseAccelerationSubscriptionParam {
-   /**
-    * A Deep client instance
-    */
-   deep: DeepClient;
-   /**
-    * A device link id
-    */
-   deviceLinkId: number;
+  /**
+   * A Deep client instance
+   */
+  deep: DeepClient;
+  /**
+   * A container link id
+   */
+  containerLinkId: number;
 }
--- src/react/hooks/use-orientation-subscription.ts
+++ src/react/hooks/use-orientation-subscription.ts
@@ -2,31 +2,43 @@ import { Motion } from "@capacitor/motion";
 import { getMotionValueUpdateSerialOperations } from "../../get-motion-value-update-serial-operationsa";
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
 import { useEffect } from "react";
+import { getSubscriptionHandler } from "../get-subscription-handler";
+import createDebugMessages from 'debug';
+import { PACKAGE_NAME } from "../../package-name";
 
 /**
  * This hook subscribes to the orientation event and saves the data to Deep
  * 
  * @remarks
  * It is not recommended to use this hook directly. Instead of {@link WithUseOrientationSubscription}
+ * 
+ * @exaple
+```ts
+useOrientationSubscription({
+   deep,
+   containerLinkId
+})
+```
  */
 export function useOrientationSubscription(param:UseOrientationSubscriptionParam) {
-   const { deep, deviceLinkId } = param;
+   const debug = createDebugMessages(`${PACKAGE_NAME}:useOrientationSubscription`)
+   debug({param})
+   const { deep, containerLinkId} = param;
 
     useEffect(() => {
-      const orientationHandler = Motion.addListener('orientation', async (orientationData) => {
-         await getMotionValueUpdateSerialOperations({
-           deep,
-           deviceLinkId,
-           info: {
-            rotationRate: orientationData
-           },
-         });
-       });
-
+      const accelerationHandlerFunction = getSubscriptionHandler({
+         deep,
+         containerLinkId,
+      })
+      const orientationHandler = Motion.addListener('orientation', (event) => {
+         accelerationHandlerFunction({
+            rotationRate: event
+         })
+      });
       return () => {
          orientationHandler.remove();
       }
-   }, [deep, deviceLinkId]) 
+   }, [deep, containerLinkId]) 
 }
 
 export interface UseOrientationSubscriptionParam {
@@ -35,7 +47,7 @@ export interface UseOrientationSubscriptionParam {
     */
    deep: DeepClient;
    /**
-    * A device link id
+    * A container link id
     */
-   deviceLinkId: number;
+   containerLinkId: number;
 }
