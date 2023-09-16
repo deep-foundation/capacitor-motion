75bd8c472eb6148f68c1aeb25de0883e4091ce7a
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-16 20:38:10 +0500

    Use esm path

--- src/react/hooks/use-permissions-request.ts
+++ src/react/hooks/use-permissions-request.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { requestPermissions,PermissionState } from "../../request-permissions";
+import { requestPermissions,PermissionState } from "../../request-permissions.js";
 
 export function usePermissionsRequest() {
   const [permissionsState, setPermissionsState] = useState<PermissionState|undefined>(undefined);
