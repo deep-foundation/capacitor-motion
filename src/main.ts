e7d8eb0f79b5c5e2ceaed617906b6bb2205d1fb8
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-27 16:16:00 +0600

    Format

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,62 @@
+export {
+  WithAccelerationSync,
+  type WithAccelerationSyncOptions,
+} from "./react/components/with-acceleration-sync.js";
+export {
+  WithMotionSync,
+  type WithMotionSyncOptions,
+} from "./react/components/with-motion-sync.js";
+export {
+  WithOperatingSystemChecking,
+  WithOperatingSystemChecking as WithMotionOperatingSystemChecking,
+  type WithOperatingSystemCheckingOptions,
+  WithOperatingSystemCheckingOptions as WithMotionOperatingSystemCheckingOptions,
+} from "./react/components/with-operating-system-checking.js";
+export {
+  WithOrientationSync,
+  type WithOrientationSyncOptions,
+} from "./react/components/with-orientation-sync.js";
+export {
+  WithPermissions,
+  WithPermissionsOptions,
+  WithPermissions as WithMotionPermissions,
+  WithPermissionsOptions as WithMotionPermissionsOptions,
+} from "./react/components/with-permissions.js";
 
-export {WithAccelerationSync ,type WithAccelerationSyncOptions } from './react/components/with-acceleration-sync.js'
-export {WithMotionSync, type WithMotionSyncOptions} from './react/components/with-motion-sync.js'
-export {WithOperatingSystemChecking, WithOperatingSystemChecking as WithMotionOperatingSystemChecking, type WithOperatingSystemCheckingOptions, WithOperatingSystemCheckingOptions as WithMotionOperatingSystemCheckingOptions} from './react/components/with-operating-system-checking.js'
-export {WithOrientationSync , type WithOrientationSyncOptions } from './react/components/with-orientation-sync.js'
-export {WithPermissions,WithPermissionsOptions, WithPermissions as WithMotionPermissions, WithPermissionsOptions as WithMotionPermissionsOptions} from './react/components/with-permissions.js'
+export {
+  useMotionSync,
+  UseMotionSyncOptions,
+} from "./react/hooks/use-motion-sync.js";
+export {
+  useAccelerationSync,
+  UseAccelerationSyncOptions,
+} from "./react/hooks/use-acceleration-sync.js";
+export {
+  useOrientationSync,
+  UseOrientationSyncOptions,
+} from "./react/hooks/use-orientation-sync.js";
+export { usePermissionsRequest } from "./react/hooks/use-permissions-request.js";
 
-export {useMotionSync, UseMotionSyncOptions} from './react/hooks/use-motion-sync.js'
-export {useAccelerationSync,UseAccelerationSyncOptions} from './react/hooks/use-acceleration-sync.js'
-export {useOrientationSync,UseOrientationSyncOptions} from './react/hooks/use-orientation-sync.js'
-export {usePermissionsRequest} from './react/hooks/use-permissions-request.js'
+export {
+  makeMotionInsertOperations,
+  type MakeMotionInsertOperationsOptions,
+} from "./make-motion-insert-operations.js";
+export {
+  makeMotionValueUpdateOperations,
+  type MakeMotionValueUpdateSerialOperationsOptions,
+} from "./make-motion-value-update-operations.js";
+export { type MotionInfo } from "./motion-info.js";
+export { requestPermissions } from "./request-permissions.js";
+export { subscribeToAccelerationChanges } from "./subscribe-to-acceleration-changes.js";
+export { subscribeToOrientationChanges } from "./subscribe-to-orientation-changes.js";
+export { type SubscribeToMotionChangesOptions } from "./subscribe-to-motion-changes-options.js";
 
-export {makeMotionInsertOperations, type MakeMotionInsertOperationsOptions} from './make-motion-insert-operations.js'
-export {makeMotionValueUpdateOperations, type MakeMotionValueUpdateSerialOperationsOptions} from './make-motion-value-update-operations.js'
-export {type MotionInfo} from './motion-info.js'
-export {requestPermissions} from './request-permissions.js'
-export {subscribeToAccelerationChanges} from './subscribe-to-acceleration-changes.js'
-export {subscribeToOrientationChanges} from './subscribe-to-orientation-changes.js'
-export {type SubscribeToMotionChangesOptions} from './subscribe-to-motion-changes-options.js'
+export { insertMotion, InsertMotionOptions } from "./insert-motion.js";
+export { updateMotion, UpdateMotionOptions } from "./update-motion.js";
 
-export {insertMotion,InsertMotionOptions} from './insert-motion.js'
-export {updateMotion,UpdateMotionOptions} from './update-motion.js'
+export {
+  MotionDecorator,
+  createMotionDecorator,
+} from "./create-motion-decorator.js";
 
-export {MotionDecorator,createMotionDecorator} from './create-motion-decorator.js'
-
-
-export {Package,PackageOptions} from './package.js'
+export { Package, PackageOptions } from "./package.js";
